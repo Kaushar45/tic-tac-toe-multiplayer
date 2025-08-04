99b90c9afd519771d4d76ac8e6917339d15cde95
Kaushar45 <45kaushar@gmail.com>
2025-08-04 21:39:25 +0530

    add restart functionality and update game state handling

--- client/script.js
+++ client/script.js
@@ -6,18 +6,39 @@ if (userId) {
 }
 
 let boxes = document.querySelectorAll(".box");
+let restartBtn = document.getElementById("restart");
+let turnElement = document.getElementById("turn");
+let winElement = document.getElementById("win");
+winElement.style.display = "none";
+turnElement.style.display = "none";
 
-socket.on("data", (data) => {
+socket.on("data", ({ data, turn }) => {
+  if (turn == userId) {
+    turnElement.style.display = "block";
+  } else {
+    turnElement.style.display = "none";
+  }
   boxes.forEach((box, i) => {
     box.innerHTML = data[i];
   });
 });
 socket.on("win", (win) => {
+  turnElement.style.display = "none";
+  winElement.innerHTML = win;
+  winElement.style.display = "block";
   console.log(win);
+});
+socket.on("restart", () => {
+  turnElement.style.display = "none";
+  winElement.style.display = "none";
 });
 
 boxes.forEach((box, i) => {
   box.addEventListener("click", function () {
     socket.emit("input", i);
   });
+});
+
+restartBtn.addEventListener("click", () => {
+  socket.emit("restart", userId);
 });
--- server/index.mjs
+++ server/index.mjs
@@ -17,28 +17,55 @@ const winPatterns = [
   [3, 4, 5],
   [6, 7, 8],
 ];
+const restartGame = () => {
+  turn = "1";
+  data = ["", "", "", "", "", "", "", "", ""];
+  io.emit("restart", "");
+};
 
 const checkWinner = () => {
+  const lastTurn = turn;
+  let isDraw = true;
   for (let pattern of winPatterns) {
     const v1 = data[pattern[0]];
     const v2 = data[pattern[1]];
     const v3 = data[pattern[2]];
+    let lastWinner = "";
     if (v1 !== "" && v2 !== "" && v3 !== "" && v1 === v2 && v2 === v3) {
       if (v1 === "0") {
         io.emit("win", "user 2 (0) is the winner");
+        lastWinner = "2";
         isWin = true;
       } else {
         io.emit("win", "user 1 (X) is the winner");
+        lastWinner = "1";
         isWin = turn;
       }
       turn = "";
       setTimeout(() => {
-        turn = "1";
-        data = ["", "", "", "", "", "", "", "", ""];
-      }, 5000);
+        restartGame();
+        turn = lastWinner;
+        io.emit("data", { data, turn });
+      }, 3000);
     }
   }
 };
+for (let value of data) {
+  if (value === "") {
+    isDraw = false;
+    break;
+  }
+}
+
+if (isDraw) {
+  io.emit("win", "match draw!!!");
+  turn = "";
+  setTimeout(() => {
+    restartGame();
+    turn = lastTurn;
+    io.emit("data", { data, turn });
+  }, 3000);
+}
 const checkMatchDraw = () => {
   if (data.every((item) => item !== "") && !isWinnerAnnounced) {
     io.emit("win", "Match Draw");
@@ -60,6 +87,7 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+  io.emit("data", { data, turn });
   socket.on("input", (e) => {
     if (e >= 0 && e < 9) {
       const clientId = clients[socket.id];
@@ -75,7 +103,7 @@ io.on("connection", (socket) => {
         checkMatchDraw();
       }
     }
-    io.emit("data", data);
+    io.emit("data", turn);
   });
   socket.on("info", (userId) => {
     if (!userId || userId > 2 || userId < 0) {
@@ -83,6 +111,15 @@ io.on("connection", (socket) => {
     }
     clients[socket.id] = userId;
     console.log(clients);
+  });
+  socket.on("restart", (turnMsg) => {
+    console.log(turnMsg);
+    console.log(`reset request from ${turnMsg}`);
+    if (turnMsg == turn) {
+      restartGame();
+      turn = turnMsg;
+      io.emit("data", { data, turn });
+    }
   });
 });
 
